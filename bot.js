ad62468ff6392cd6bec5cf3891d66c97da225049
ShayanSolid <31272536+ShayanSolid@users.noreply.github.com>
2018-06-08 16:08:36 +0200

    remaining time completed

--- bot.js
+++ bot.js
@@ -4,36 +4,39 @@ var Twit = require('twit');
 var config = require ('./config.js');
 var T = new Twit(config);
 
-setInterval(tweetIt, 1000*10*60)
+setInterval(tweetIt, 1000*10*60*60)
 
 tweetIt();
 
 function tweetIt(){
-  var r = Math.floor(Math.random()*100);
   var tweet = {
-    status: 'here is a random number ' + r + ' #shayan_twitter_bot'
+    status: 'Remaining time to the next PoGo ' + timeToNextPogo() + ' #WhoWinnsThePot'
   }
-
+  // console.log("tweet: ", tweet);
   T.post('statuses/update',  tweet, tweeted)
-
   function tweeted(err,data,response){
     if(err){
-      console.log("something went wrong!");
+      console.log("something went wrong!", err);
     }else{
       console.log("it workedd !");
     }
   }
 }
 
-//search
-// var params ={
-//   q: 'trump',
-//   count: 5
-// }
-// T.get('search/tweets', params, goData);
-// function goData(err, data, response) {
-//   var tweets = data.statuses;
-//   for(var i=0 ; i< tweets.length ; i++){
-//     console.log(tweets[i].text);
-//   }
-// }
+function timeToNextPogo(){
+  var d = new Date;
+  var nextPogoTime = new Date("Jun 15, 2018 16:00:00");
+  var remainingTime = nextPogoTime - d;
+  return convertToReadableTime(remainingTime);
+}
+
+function convertToReadableTime(time){
+    var d= new Date();
+    var day=1000*60*60*24
+    var days = Math.round(time/day);
+    var h = d.getHours();
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+    var readableTime = days + "days " + h + " hours " + m + " minutes " + s + " seconds";
+    return readableTime;
+}
